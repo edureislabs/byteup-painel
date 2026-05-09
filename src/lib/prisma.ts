5d7a2cd7834f77dad937ae1535f6b7f5153cf58f
Eduardo <204800370+edureislabs@users.noreply.github.com>
2026-05-09 02:31:04 -0300

    adiciona postinstall e tipos prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,13 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaNeonHttp } from '@prisma/adapter-neon'
+
+// Use a string de conexão diretamente (já está no .env)
+const adapter = new PrismaNeonHttp(process.env.DATABASE_URL!, {})
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter } as any)
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
