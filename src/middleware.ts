519b2cdb6d4e26f7db4faa5357765e4bb326211f
Eduardo <204800370+edureislabs@users.noreply.github.com>
2026-05-10 01:21:47 -0300

    adiciona postinstall e tipos prisma

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,14 +2,8 @@ import { getToken } from "next-auth/jwt"
 import { NextRequest, NextResponse } from "next/server"
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({
-    req,
-    secret: process.env.NEXTAUTH_SECRET,
-    cookieName:
-      process.env.NODE_ENV === "production"
-        ? "__Secure-next-auth.session-token"
-        : "next-auth.session-token",
-  })
+  // Deixe de fora o cookieName, o NextAuth usa o padrão correto
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
 
   if (!token) {
     const loginUrl = new URL("/login", req.url)
