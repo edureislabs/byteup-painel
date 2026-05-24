13779035f62c89a60d5ea28c9a5b4dcc60db37a3
Eduardo <204800370+edureislabs@users.noreply.github.com>
2026-05-24 14:12:32 -0300

    atualização sistema de segurança

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,17 @@
-import { getToken } from "next-auth/jwt"
-import { NextRequest, NextResponse } from "next/server"
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  // Deixe de fora o cookieName, o NextAuth usa o padrão correto
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
   if (!token) {
-    const loginUrl = new URL("/login", req.url)
-    return NextResponse.redirect(loginUrl)
+    const loginUrl = new URL("/login", req.url);
+    return NextResponse.redirect(loginUrl);
   }
 
-  return NextResponse.next()
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
-}
+  matcher: ["/dashboard/:path*", "/api/guilds/:path*"],
+};
